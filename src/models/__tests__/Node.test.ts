bae740a08cfc45e620e48da817c70ded05b4504e
geolffreym <gmjun2000@gmail.com>
2022-10-13 17:10:49 -0600

    test: add null test for invalid LCA

--- src/models/__tests__/Node.test.ts
+++ src/models/__tests__/Node.test.ts
@@ -35,12 +35,19 @@ describe('deserializes BT correctly', () => {
     expect(preOrder).toStrictEqual([3, 9, 2, 6, 7, 4]);
   });
 
-  it('should find the right LCA based on root, p,q', () => {
+  it('should find the LCA based on root, p,q', () => {
     const stringArray = '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]';
     const tree = deserializeBT(JSON.parse(stringArray));
 
-    expect(LCA(tree, 18, 19)).toStrictEqual(9)
-    expect(LCA(tree, 10, 15)).toStrictEqual(1)
+    expect(LCA(tree, 18, 19)?.value).toStrictEqual(9)
+    expect(LCA(tree, 10, 15)?.value).toStrictEqual(1)
+  })
+
+  it('should return null for invalid LCA based on root, p,q', () => {
+    const stringArray = '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]';
+    const tree = deserializeBT(JSON.parse(stringArray));
+
+    expect(LCA(tree, 100,50)).toStrictEqual(null)
   })
 
 });
